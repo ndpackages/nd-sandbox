47c4078bf05e516621ab70d1f6c2344a3d7f1291
vitaliy <zndev@ya.ru>
2022-09-03 00:07:52 +0600

    upd

---
+++ web/widgets/imageSliderWidget.tsx
@@ -0,0 +1,14 @@
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css";
+import React from "react";
+
+function ImageSliderWidget(props) {
+
+    return (
+        <ImageGallery {...props} />
+    );
+}
+
+export default ImageSliderWidget;
+
+// https://www.npmjs.com/package/react-image-gallery

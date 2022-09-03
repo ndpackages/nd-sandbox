9bc940c99d2e2a95ddb9a99051b836cbb1ef81f6
vitaliy <zndev@ya.ru>
2022-09-03 17:47:09 +0600

    upd

--- core/helpers/encoders/Utf8Helper.ts
+++ core/helpers/encoders/Utf8Helper.ts
@@ -2,11 +2,11 @@ let aesjs = require('aes-js');
 
 export default class Utf8Helper {
 
-    static encode(binary) {
+    /*static encode(binary) {
         return aesjs.utils.utf8.fromBytes(binary);
     }
 
     static decode(utf8) {
         return aesjs.utils.utf8.toBytes(utf8);
-    }
+    }*/
 }

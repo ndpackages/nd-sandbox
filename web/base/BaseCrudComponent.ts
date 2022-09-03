330eb2b8d57914b70a162c6030924c1b4eeb10b8
vitaliy <zndev@ya.ru>
2022-09-04 00:23:40 +0600

    upd

--- web/base/BaseCrudComponent.ts
+++
@@ -1,8 +0,0 @@
-import BaseCrudService from "../../../bundlesCore/domain/services/BaseCrudService";
-import BaseActiveComponent from "./BaseActiveComponent";
-
-export default class BaseCrudComponent extends BaseActiveComponent {
-
-    protected service: BaseCrudService;
-
-}

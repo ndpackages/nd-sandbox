b9085b2f968182b74cec61fa4dbd733e594e5673
vitaliy <zndev@ya.ru>
2022-09-03 17:30:54 +0600

    upd

--- core/helpers/encoders/Base64Helper.ts
+++ core/helpers/encoders/Base64Helper.ts
@@ -1,9 +1,13 @@
 import {Base64} from 'js-base64';
+import Base64Encoder from "../../../../bundlesExt/baseX/libs/Base64Encoder";
 
 export default class Base64Helper {
 
     static encode(data) {
-        return Base64.encode(data);
+        let baseXEncoder = new Base64Encoder();
+        return baseXEncoder.encode(data);
+
+        // return Base64.encode(data);
     }
 
     static decode(base64) {

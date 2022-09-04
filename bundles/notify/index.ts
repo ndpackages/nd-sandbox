08b80348cd626edb7e6477d83845a963e5864623
vitaliy <zndev@ya.ru>
2022-09-04 23:29:48 +0600

    upd

--- bundles/notify/index.ts
+++ bundles/notify/index.ts
@@ -1,4 +1,5 @@
 import configureDi from "./config/di";
+import 'react-toastify/dist/ReactToastify.css';
 
 let domainName = 'notify';
 configureDi(domainName);
--- widgets/imageSlider/ImageSliderWidget.tsx
+++ widgets/imageSlider/ImageSliderWidget.tsx
@@ -2,11 +2,4 @@ import ImageGallery from 'react-image-gallery';
 import "react-image-gallery/styles/css/image-gallery.css";
 import React from "react";
 
-function ImageSliderWidget(props) {
-
-    return (
-        <ImageGallery {...props} />
-    );
-}
-
-export default ImageSliderWidget;
+export default ImageGallery;

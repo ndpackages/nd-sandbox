39c6d79a9eec40cf9a9e3f709694ccc1ae6cb284
vitaliy <zndev@ya.ru>
2022-09-04 19:03:18 +0600

    upd

--- ext/hash/helpers/HashHelper.ts
+++ ext/hash/helpers/HashHelper.ts
@@ -7,19 +7,21 @@ import Sha1Hasher from "../libs/hashers/hash/Sha1Hasher";
 import Sha512Hasher from "../libs/hashers/hash/Sha512Hasher";
 import Sha3Hasher from "../libs/hashers/hash/Sha3Hasher";
 import Md5Hasher from "../libs/hashers/hash/Md5Hasher";
+import PolimorphicHasher from "../libs/hashers/hash/PolimorphicHasher";
 
 export default class HashHelper {
 
     protected static classMap = {};
 
     static hash(algorithm, value) {
-        let hasher = this.createHasherByAlgorithm(algorithm);
+        // let hasher = this.createHasherByAlgorithm(algorithm);
+        let hasher = new PolimorphicHasher(algorithm);
         return hasher.encode(value);
 
         // return hash(algorithm, value);
     }
 
-    static initClassMap() {
+    /*static initClassMap() {
         if(_.isEmpty(this.classMap)) {
             this.classMap[HashAlgorithmEnum.SHA1] = Sha1Hasher;
             this.classMap[HashAlgorithmEnum.SHA256] = Sha256Hasher;
@@ -30,44 +32,36 @@ export default class HashHelper {
         }
     }
 
+    static addAlgorithmClass(algorithm, className) {
+        this.initClassMap();
+        this.classMap[algorithm] = className;
+    }
+
     static getClassMap() {
         this.initClassMap();
         return this.classMap;
-    }
+    }*/
 
     static createHasherByAlgorithm(algorithm) {
-        let map = this.getClassMap();
-        let hasherClass = map[algorithm];
-        let hasher = new hasherClass();
+        let hasher = new PolimorphicHasher(algorithm);
 
-
-        /*if(algorithm == HashAlgorithmEnum.SHA1) {
-            hasher = new Sha1Hasher();
-        } else if(algorithm == HashAlgorithmEnum.SHA256) {
-            hasher = new Sha256Hasher();
-        } else if(algorithm == HashAlgorithmEnum.SHA512) {
-            hasher = new Sha512Hasher();
-        } else if(algorithm == HashAlgorithmEnum.SHA3) {
-            hasher = new Sha3Hasher();
-        } else if(algorithm == HashAlgorithmEnum.RIPEMD160) {
-            hasher = new Ripemd160Hasher();
-        } else if(algorithm == HashAlgorithmEnum.MD5) {
-            hasher = new Md5Hasher();
-        }*/
-        return hasher;
+        // let map = this.getClassMap();
+        // let hasherClass = map[algorithm];
+        // let hasher = new hasherClass();
+        return hasher.getHasherInstance();
     }
 
     static sha256(value) {
-        let hasher = new Sha256Hasher();
-        return hasher.encode(value);
+        // let hasher = new Sha256Hasher();
+        // return hasher.encode(value);
 
-        // return hash(HashAlgorithmEnum.SHA256, value);
+        return this.hash(HashAlgorithmEnum.SHA256, value);
     }
 
     static ripemd160(value) {
-        let hasher = new Ripemd160Hasher();
-        return hasher.encode(value);
+        // let hasher = new Ripemd160Hasher();
+        // return hasher.encode(value);
 
-        // return hash(HashAlgorithmEnum.RIPEMD160, value);
+        return this.hash(HashAlgorithmEnum.RIPEMD160, value);
     }
 }
---
+++ ext/hash/libs/hashers/hash/PolimorphicHasher.ts
@@ -0,0 +1,58 @@
+import BaseHasher from "../BaseHasher";
+import HashAlgorithmEnum from "../../../enums/HashAlgorithmEnum";
+import Sha1Hasher from "./Sha1Hasher";
+import Sha256Hasher from "./Sha256Hasher";
+import Sha512Hasher from "./Sha512Hasher";
+import Sha3Hasher from "./Sha3Hasher";
+import Ripemd160Hasher from "./Ripemd160Hasher";
+import Md5Hasher from "./Md5Hasher";
+import _ from 'lodash';
+
+export default class PolimorphicHasher extends BaseHasher {
+
+    protected classMap: object = {};
+    protected algorithm: string;
+
+    constructor(algorithm: string, classMap: object = {}) {
+        super();
+        this.algorithm = algorithm;
+        let allClassMap = PolimorphicHasher.getDefaultClassMap();
+        if (!_.isEmpty(classMap)) {
+            allClassMap = _.merge(allClassMap, classMap);
+        }
+        this.classMap = allClassMap;
+    }
+
+    encode(value) {
+        let hasher = this.getHasherInstance();
+        return hasher.encode(value);
+    }
+
+    getAlgorithm(): string {
+        return this.algorithm;
+    }
+
+    addAlgorithmClass(algorithm, className) {
+        this.classMap[algorithm] = className;
+    }
+
+    getHasherInstance() {
+        return this.createHasherByAlgorithm(this.algorithm);
+    }
+
+    private static getDefaultClassMap() {
+        let map = {};
+        map[HashAlgorithmEnum.SHA1] = Sha1Hasher;
+        map[HashAlgorithmEnum.SHA256] = Sha256Hasher;
+        map[HashAlgorithmEnum.SHA512] = Sha512Hasher;
+        map[HashAlgorithmEnum.SHA3] = Sha3Hasher;
+        map[HashAlgorithmEnum.RIPEMD160] = Ripemd160Hasher;
+        map[HashAlgorithmEnum.MD5] = Md5Hasher;
+        return map;
+    }
+
+    private createHasherByAlgorithm(algorithm) {
+        let hasherClass = this.classMap[algorithm];
+        return new hasherClass();
+    }
+}

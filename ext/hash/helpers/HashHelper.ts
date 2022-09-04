c4856d2695bcac81b63172dc970ed6641d150874
vitaliy <zndev@ya.ru>
2022-09-04 18:34:00 +0600

    upd

--- ext/hash/helpers/HashHelper.ts
+++ ext/hash/helpers/HashHelper.ts
@@ -2,11 +2,37 @@ import hash from 'crypto-hashing';
 import HashAlgorithmEnum from "../enums/HashAlgorithmEnum";
 import Sha256Hasher from "../libs/hashers/hash/Sha256Hasher";
 import Ripemd160Hasher from "../libs/hashers/hash/Ripemd160Hasher";
+import Sha1Hasher from "../libs/hashers/hash/Sha1Hasher";
+import Sha512Hasher from "../libs/hashers/hash/Sha512Hasher";
+import Sha3Hasher from "../libs/hashers/hash/Sha3Hasher";
+import Md5Hasher from "../libs/hashers/hash/Md5Hasher";
 
 export default class HashHelper {
 
     static hash(algorithm, value) {
-        return hash(algorithm, value);
+        let hasher = this.createHasherByAlgorithm(algorithm);
+        return hasher.encode(value);
+
+        // return hash(algorithm, value);
+    }
+
+    static createHasherByAlgorithm(algorithm) {
+        let hasher;
+
+        if(algorithm == HashAlgorithmEnum.SHA1) {
+            hasher = new Sha1Hasher();
+        } else if(algorithm == HashAlgorithmEnum.SHA256) {
+            hasher = new Sha256Hasher();
+        } else if(algorithm == HashAlgorithmEnum.SHA512) {
+            hasher = new Sha512Hasher();
+        } else if(algorithm == HashAlgorithmEnum.SHA3) {
+            hasher = new Sha3Hasher();
+        } else if(algorithm == HashAlgorithmEnum.RIPEMD160) {
+            hasher = new Ripemd160Hasher();
+        } else if(algorithm == HashAlgorithmEnum.MD5) {
+            hasher = new Md5Hasher();
+        }
+        return hasher;
     }
 
     static sha256(value) {

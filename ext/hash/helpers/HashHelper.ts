c062801f5323dc3029886da0974603f281e4799a
vitaliy <zndev@ya.ru>
2022-09-04 18:42:29 +0600

    upd

--- ext/hash/helpers/HashHelper.ts
+++ ext/hash/helpers/HashHelper.ts
@@ -1,4 +1,5 @@
 import hash from 'crypto-hashing';
+import _ from 'lodash';
 import HashAlgorithmEnum from "../enums/HashAlgorithmEnum";
 import Sha256Hasher from "../libs/hashers/hash/Sha256Hasher";
 import Ripemd160Hasher from "../libs/hashers/hash/Ripemd160Hasher";
@@ -9,6 +10,8 @@ import Md5Hasher from "../libs/hashers/hash/Md5Hasher";
 
 export default class HashHelper {
 
+    protected static classMap = {};
+
     static hash(algorithm, value) {
         let hasher = this.createHasherByAlgorithm(algorithm);
         return hasher.encode(value);
@@ -16,10 +19,29 @@ export default class HashHelper {
         // return hash(algorithm, value);
     }
 
-    static createHasherByAlgorithm(algorithm) {
-        let hasher;
+    static initClassMap() {
+        if(_.isEmpty(this.classMap)) {
+            this.classMap[HashAlgorithmEnum.SHA1] = Sha1Hasher;
+            this.classMap[HashAlgorithmEnum.SHA256] = Sha256Hasher;
+            this.classMap[HashAlgorithmEnum.SHA512] = Sha512Hasher;
+            this.classMap[HashAlgorithmEnum.SHA3] = Sha3Hasher;
+            this.classMap[HashAlgorithmEnum.RIPEMD160] = Ripemd160Hasher;
+            this.classMap[HashAlgorithmEnum.MD5] = Md5Hasher;
+        }
+    }
 
-        if(algorithm == HashAlgorithmEnum.SHA1) {
+    static getClassMap() {
+        this.initClassMap();
+        return this.classMap;
+    }
+
+    static createHasherByAlgorithm(algorithm) {
+        let map = this.getClassMap();
+        let hasherClass = map[algorithm];
+        let hasher = new hasherClass();
+
+
+        /*if(algorithm == HashAlgorithmEnum.SHA1) {
             hasher = new Sha1Hasher();
         } else if(algorithm == HashAlgorithmEnum.SHA256) {
             hasher = new Sha256Hasher();
@@ -31,7 +53,7 @@ export default class HashHelper {
             hasher = new Ripemd160Hasher();
         } else if(algorithm == HashAlgorithmEnum.MD5) {
             hasher = new Md5Hasher();
-        }
+        }*/
         return hasher;
     }
 

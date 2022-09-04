8d241b3e85d4cc731b7d545d5b47af420fe4555a
vitaliy <zndev@ya.ru>
2022-09-04 13:59:03 +0600

    upd

--- core/configManager/libs/BaseConfigManager.ts
+++ core/configManager/libs/BaseConfigManager.ts
@@ -2,21 +2,24 @@ import _ from 'lodash';
 
 export default class BaseConfigManager {
 
-    protected data = {};
+    protected config = {};
 
-    set(path, value) {
-        _.set(this.data, path, value);
-    }
+    /*set(path, value) {
+        _.set(this.config, path, value);
+    }*/
 
     get(path, defaultValue = null) {
-        return _.get(this.data, path, defaultValue);
+        return _.get(this.config, path, defaultValue);
     }
 
     load(config) {
-        this.data = config;
+        if(!_.isEmpty()) {
+            throw new Error('Config already loaded! Config can not override!');
+        }
+        this.config = config;
     }
 
-    all() {
-        return this.data;
-    }
+    /*all() {
+        return this.config;
+    }*/
 }
